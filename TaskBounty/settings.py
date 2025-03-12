2d36e5b71d98ddaeb423afef1afff9725407d6ac
GautmSahu <gautamsahu770@gmail.com>
2025-03-12 21:22:49 +0530

    undo changes

--- TaskBounty/settings.py
+++ TaskBounty/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/opt/render/project/src/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
